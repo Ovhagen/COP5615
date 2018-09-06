949ebaa048cc4efa3aacd523d604bca0a15611cc
Pontus Ovhagen <ovhagen@gmail.com>
2018-09-06 15:16:07 -0400

    Added simple test for perfect square functions. Run easily with mix test

--- proj1/test/proj1_test.exs
+++ proj1/test/proj1_test.exs
@@ -2,7 +2,14 @@ defmodule Proj1Test do
   use ExUnit.Case
   doctest Proj1
 
-  test "greets the world" do
-    assert Proj1.hello() == :world
+  @doc """
+  Individual tests for functions of finding perfect squares.
+  """
+  test "SumSquare test" do
+    IO.puts "Running square tests..."
+    assert SqSum.square_sums(1, 3, 2) |> SqSum.find_squares(1) == [3]
+    assert SqSum.square_sums(1, 40, 24) |> SqSum.find_squares(1) == [1, 9, 20, 25]
+    assert SqSum.square_sums(1, 1000000, 409) |> SqSum.find_squares(1) == [71752]
+    assert SqSum.square_sums(1, 10000000, 409) |> SqSum.find_squares(1) == [71752, 1236640]
   end
 end

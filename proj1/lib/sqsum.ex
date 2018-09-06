7260ff4fe267c0fa87997948bbc8c8ab8a5ba8e7
jotakami <jotakami@gmail.com>
2018-09-05 22:05:39 -0400

    Completed SqSum module
    The SqSum module now has all the functions needed to calculate sums of squares and check for perfect squares. They run fast :-)
    
    Example test:
    SqSum.square_sums(1, 1000000, 409) |> SqSum.find_squares(1)

--- proj1/lib/sqsum.ex
+++ proj1/lib/sqsum.ex
@@ -18,11 +18,35 @@ defmodule SqSum do
   
   """
   def next_sum(start, finish, length, n, last_sum) when n >= finish - start + length - 1 do
-    last_sum + length*(2*n + 2*start - length)
+    [last_sum + length*(2*n + 2*start - length)]
   end
   
   def next_sum(start, finish, length, n, last_sum) do
     sum = last_sum + length*(2*n + 2*start - length)
 	[sum | next_sum(start, finish, length, n+1, sum)]
+  end
+  
+  @doc """
+  Finds perfect squares in a list of consecutive square sums
+  
+  """
+  def find_squares(sums, start) do
+    n = Kernel.trunc(:math.sqrt(hd(sums)))
+    find_next(sums, start, n, n*n)
+  end
+  
+  def find_next([sum | sums], n, k, target) do
+    cond do
+	  sum > target ->
+	    find_next([sum | sums], n, k+1, target + 2*k + 1)
+	  sum < target ->
+	    find_next(sums, n+1, k, target)
+	  sum == target ->
+	    [n | find_next(sums, n+1, k+1, target + 2*k + 1)]
+	end
+  end
+  
+  def find_next(sum, n, k, target) do
+    if sum == target, do: [n], else: []
   end
 end

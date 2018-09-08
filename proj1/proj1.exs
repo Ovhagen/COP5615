c1572f48f3f6d60558d3b4541aac47a5c0d4823f
jotakami <jotakami@gmail.com>
2018-09-07 22:40:37 -0400

    Integrated timing
    Timing is built-in to proj1.exs now. At the end of the output, it will show elapsed CPU time and clock time.

--- proj1/proj1.exs
+++ proj1/proj1.exs
@@ -1,14 +1,16 @@
 [space, length] = Enum.map_every(System.argv, 1, fn(arg) -> String.to_integer(arg) end)
 processes = System.schedulers_online
-time = :timer.tc( fn ->
+{clock_time, {cpu_time, results}} = :timer.tc(fn ->
   (for n <- 0..processes-1, do: {round(n*space/processes + 1), round((n+1)*space/processes), length})
     |> Task.async_stream(fn {a, b, c} ->
-	  SqSum.square_sums(a, b, c)
-        |> SqSum.find_squares(a) end, timeout: 60000)
-    |> Enum.reduce([], fn {:ok, x}, acc -> acc ++ x end)
-    |> Enum.each(fn x -> IO.puts x end)
+	  :timer.tc(fn a, b, c ->
+	    SqSum.square_sums(a, b, c)
+          |> SqSum.find_squares(a) end,
+		[a, b, c]) end,
+	  timeout: 60000)
+    |> Enum.reduce({0, []}, fn {:ok, {time, result}}, {cpu_time, results} ->
+	  {cpu_time + time, results ++ result} end)
   end)
-  |> elem(0)
-  |> Kernel./(1_000)
-  |> round()
-IO.puts "Running time: #{time} ms"
+Enum.each(results, fn x -> IO.puts x end)
+IO.puts "CPU time:   #{cpu_time |> Kernel./(1000) |> round()} ms"
+IO.puts "Clock time: #{clock_time |> Kernel./(1000) |> round()} ms"

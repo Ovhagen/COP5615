f04551b2c03b1094710d75bbfc701ef66ff5d5d5
Pontus Ovhagen <ovhagen@gmail.com>
2018-09-08 14:02:12 -0400

    Saved and removed old files. Fixed division by zero case for lower orders of input.#

--- proj1/proj1.exs
+++ proj1/proj1.exs
@@ -32,4 +32,8 @@ chunks = max(1, round(space/200_000_000))*System.schedulers_online
 Enum.each(results, fn x -> IO.puts x end)
 IO.puts "CPU time:   #{cpu_time |> Kernel./(1000) |> round()} ms"
 IO.puts "Clock time: #{clock_time |> Kernel./(1000) |> round()} ms"
-IO.puts "Ratio: #{cpu_time/clock_time}"
+if clock_time == 0 do
+  IO.puts "Ratio: #{0}"
+else
+  IO.puts "Ratio: #{cpu_time/clock_time}"
+end

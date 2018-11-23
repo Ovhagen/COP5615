57bf7191408a87cd2c9b4040ced3b8431540d19f
jotakami <jotakami@gmail.com>
2018-11-23 00:06:50 -0500

    Key and address functions
    Finished functions to generate key pairs, compress public keys, generate public key hashes, and generate bitcoin addresses

---
+++ proj4/lib/jbok.ex
@@ -0,0 +1,20 @@
+defmodule JBOK do
+  defstruct [:pub_key, :pkh, :addr]
+  @type priv_key :: <<_::256>>
+  @type pub_key :: <<_::520>>
+  @type pkh :: <<_::160>>
+  @type addr :: String.t
+  
+  use Agent
+  
+  def start_link(_), do: Agent.start_link(fn -> %{} end)
+  
+  def generate_key(pid) do
+    {pub_key, priv_key} = KeyAddress.keypair
+    pkh = KeyAddress.compress_pubkey(pub_key)
+      |> KeyAddress.pubkey_to_pkh
+    addr = KeyAddress.pkh_to_addr(pkh)
+    :ok = Agent.update(pid, Map, :put, [priv_key, %JBOK{pub_key: pub_key, pkh: pkh, addr: addr}])
+    priv_key
+  end
+end
--- proj4/lib/keyaddress.ex
+++ proj4/lib/keyaddress.ex
@@ -1,33 +1,45 @@
 defmodule KeyAddress do
-  @private_key "80"
-  @public_key  "04"
-  @cpk_even    "02"
-  @cpk_odd     "03"
-  @address     "00"
+  @private_key 0x80
+  @public_key  0x04
+  @cpk_even    0x02
+  @cpk_odd     0x03
+  @address     0x00
+  @wifc_suffix 0x01
   
-  def keypair() do
-    with {public_key, private_key} <- :crypto.generate_key(:ecdh, :secp256k1),
-      do: {Base.encode16(public_key), Base.encode16(private_key)}
-  end
-  def keypair(key) when is_binary(key), do: keypair(Base.decode16(key))
-  def keypair(key) do
-    with {public_key, private_key} <- :crypto.generate_key(:ecdh, :secp256k1, key),
-      do: {Base.encode16(public_key), Base.encode16(private_key)}
-  end
+  def keypair(), do: :crypto.generate_key(:ecdh, :secp256k1)
+  def keypair(key) when is_binary(key), do: keypair(:binary.decode_unsigned(key))
+  def keypair(key) when is_integer(key), do: :crypto.generate_key(:ecdh, :secp256k1, key)
   
-  def compress_pubkey(key) do
-    <<_::binary-size(1), x::binary-size(32), _::binary-size(31), flag::integer>> = key
-    (if rem(flag, 2) == 1, do: @cpk_odd, else: @cpk_even) <> x
-  end
+  def wif(key) when is_binary(key), do: Base58Check.encode(<<@private_key>>, key)
   
-  def uncompress_pubkey(cpk) do
-    # implement
+  def wifc(key) when is_binary(key), do: Base58Check.encode(<<@private_key>>, key <> <<@wifc_suffix>>)
+  
+  def compress_pubkey(<<@public_key::8, x::256, y::256>>) do
+    <<
+      (if rem(y, 2) == 1, do: @cpk_odd, else: @cpk_even)::8,
+      x::256
+    >>
   end
   
-  def pubkey_to_pkh(key) when is_binary(key), do: pubkey_to_pkh(Base.decode16(key))
-  def pubkey_to_pkh(key), do: :crypto.hash(:ripemd160, :crypto.hash(:sha256, key))
+  def uncompress_pubkey(<<prefix::integer, x::256>>) do
+    {{_, <<p::integer-256>>}, {<<a::integer>>, <<b::integer>>, _}, _, _, _} = :crypto.ec_curve(:secp256k1)
+    y = modpow(x, 3, p) + a * modpow(x, 2, p) + b
+      |> rem(p)
+      |> modpow(div(p+1, 4), p)
+    <<
+      @public_key::8,
+      x::256,
+      (if rem(y, 2) == rem(prefix, 2), do: y, else: p - y)::256
+    >>
+  end
   
-  def pkh_to_address(pkh) when is_binary(pkh), do: Base58Check.encode(<<0x00>>, pkh)
+  def pubkey_to_pkh(key) when is_binary(key), do: :crypto.hash(:ripemd160, :crypto.hash(:sha256, key))
+  
+  def pkh_to_address(pkh) when is_binary(pkh), do: Base58Check.encode(<<@address>>, pkh)
   
   def address_to_pkh(addr) when is_binary(addr), do: Base58Check.decode(addr) |> elem(1)
+  
+  defp modpow(_n, 0, _p), do: 1
+  defp modpow(n, k, p) when rem(k, 2) == 0, do: modpow(rem(n*n, p), div(k, 2), p)
+  defp modpow(n, k, p), do: rem(n * modpow(n, k-1, p), p)
 end

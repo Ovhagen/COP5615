564f9e5463b32c4ac73e90f11df93f66f5ac81b1
Pontus Ovhagen <ovhagen@gmail.com>
2018-11-27 00:23:32 -0500

    Commented tests and added real transaction tests instead of SHA1

--- proj4/test/Proj4/merkle_proof_test.exs
+++ proj4/test/Proj4/merkle_proof_test.exs
@@ -1,34 +1,49 @@
 defmodule Proj4.MerkleProofTest do
   use ExUnit.Case
+  @moduledoc """
+  This module defines a test for operations and functions in 'merkle_tree_proof.ex'.
+  """
 
   setup do
     %{
-	  tx_test: ["3e4bb40f066d195155e74eb0d26d644fbf5cab91",  #Taken from a blocks transaction list
-                "ca3bce4f810bca6f68fcecd1b79627c06016f142",
-                "ced1f2728fe4e928716a639cda1333af67eafeea",
-                "0710260689d3f95eb18bdfb0235ffcf4cd728045"],
-      target_tx: "3e4bb40f066d195155e74eb0d26d644fbf5cab91",
+    tx_test: ["c5246e478e0a319c099f94fe8e5b8c7b767a017f55408ebde477d0578659b4a8",
+                "9ac0240b5e74ebaf38efa72e105cfeb06ee119dae56954dbd4f76cd790c738a5",
+                "f25a13422d5cc7bb4331607502360cba53ceb6977623b3b4759fe96e42fdb1c8",
+                "ce249be4a7f827ebccf51beb3d4cc919ee03b2aa51e4a3444142568d46a31a8a"],
+      target_tx: "c5246e478e0a319c099f94fe8e5b8c7b767a017f55408ebde477d0578659b4a8",
       target_idx: 0,  #Given by target txs position in tx list
-      merkle_path: ["7853d08f19cbdec01cb95613771670650b2967aafbc02cf7fdd69047551fa465",
-                   "ffe1f2421d57dc07f5f0c13b439ad80cff78a0f5683a5faa9d0fab4d1bc92a2a",
-                   "fc73efaf5dae1dca1c1bdf0c3d2f59dec282a3951f42524fabe1da0e49278518",
-                   "bae2b3a1a01b4e555b9566f09e541661239c3199e9f2819af5d8563bce13ddd4"],
-    target_tx_fail: "3e4bb40f066d195155e74eb0d26d644fbf5cab9"  #Removed one letter
+      merkle_path: ["857d6e05018621948c70e1153562536144f41dd8adbe368280a9b4e27b794997",
+                   "161a1cc2a5cf1470f8abe426df4161c636be48cc0708e90ad712fa049bedab58",
+                   "4b468db712f4a9d2b14fa5bdf1d2149554cac6ee6d2b197fd4fa5560c56fa35c",
+                   "ffd2716e4f1f776214b3cd1713f2fa1b8714c775a1654a6ad6fe7372e5ee475a"],
+    target_tx_fail: "c5246e478e0a319c099f94fe8e5b8c7b767a017f55408ebde477d0578659b4a7"  #Changed one letter
 	 }
   end
 
+  @doc """
+  Test that the creation of a merkle proof is done correctly by comparing with the merkle path solution.
+  The answer was taken from hashes of the merkle tree nodes.
+  """
   test "Use case when creating a proof object", %{tx_test: tx_test, target_tx: target_tx, target_idx: target_idx, merkle_path: merkle_path} do
     tree = MerkleTree.makeMerkle(tx_test)
     proof = MerkleTree.Proof.generateMerkleProof(tree, target_tx, tree.root.height, target_idx, length(tx_test))
     assert(proof.hash_values == merkle_path)
   end
 
+  @doc """
+  This test certifies that a transaction can be successfully verified to have been included in a block. The
+  verification process is carried out by hasing up the merkle path and finally compare with the merkle root.
+  """
   test "Successfully verifying a transaction", %{tx_test: tx_test, target_tx: target_tx, target_idx: target_idx} do
     tree = MerkleTree.makeMerkle(tx_test)
     proof = MerkleTree.Proof.generateMerkleProof(tree, target_tx, tree.root.height, target_idx, length(tx_test))
     assert(MerkleTree.Proof.verify_transaction(proof))
   end
 
+  @doc """
+  This test certifies that a transaction can be successfully verified to have been included in a block. The
+  verification process is carried out by hasing up the merkle path and finally compare with the merkle root.
+  """
   test "Failing to verifying a transaction", %{tx_test: tx_test, target_idx: target_idx, target_tx_fail: target_tx_fail} do
     tree = MerkleTree.makeMerkle(tx_test)
     assert_raise(MerkleTree.ProofError, fn () -> MerkleTree.Proof.generateMerkleProof(tree, target_tx_fail, tree.root.height, target_idx, length(tx_test)) end)
--- proj4/test/Proj4/merkle_tree_test.exs
+++ proj4/test/Proj4/merkle_tree_test.exs
@@ -1,22 +1,37 @@
 defmodule Proj4.MerkleTreeTest do
   use ExUnit.Case
+  @moduledoc """
+  This module defines a test for operations and functions in 'merkle_tree.ex'.
+  """
 
   setup do
     %{
-	  tx_test1: ["3e4bb40f066d195155e74eb0d26d644fbf5cab91", "ca3bce4f810bca6f68fcecd1b79627c06016f142", "ced1f2728fe4e928716a639cda1333af67eafeea", "0710260689d3f95eb18bdfb0235ffcf4cd728045"],
-    arity_test: ["3e4bb40f066d195155e74eb0d26d644fbf5cab91", "ca3bce4f810bca6f68fcecd1b79627c06016f142", "ced1f2728fe4e928716a639cda1333af67eafeea"]
+      tx_test1: ["c5246e478e0a319c099f94fe8e5b8c7b767a017f55408ebde477d0578659b4a8",  #Transactions gathered by a miner.
+                  "9ac0240b5e74ebaf38efa72e105cfeb06ee119dae56954dbd4f76cd790c738a5",
+                  "f25a13422d5cc7bb4331607502360cba53ceb6977623b3b4759fe96e42fdb1c8",
+                  "ce249be4a7f827ebccf51beb3d4cc919ee03b2aa51e4a3444142568d46a31a8a"],
+    arity_test: ["c5246e478e0a319c099f94fe8e5b8c7b767a017f55408ebde477d0578659b4a8", "9ac0240b5e74ebaf38efa72e105cfeb06ee119dae56954dbd4f76cd790c738a5", "f25a13422d5cc7bb4331607502360cba53ceb6977623b3b4759fe96e42fdb1c8"]
 	 }
   end
 
+  @doc """
+  Test creates a merkle tree and checks that the root is correct.
+  """
   test "General use case", %{tx_test1: tx_test1} do
     tree = MerkleTree.makeMerkle(tx_test1)
-    assert(tree.root.hash_value == "bae2b3a1a01b4e555b9566f09e541661239c3199e9f2819af5d8563bce13ddd4")
+    assert(tree.root.hash_value == "ffd2716e4f1f776214b3cd1713f2fa1b8714c775a1654a6ad6fe7372e5ee475a")
   end
 
+  @doc """
+  Test that a failure is thrown when the number of transactions are not a power of 2.
+  """
   test "Power arity failure", %{arity_test: arity_test} do
     assert_raise(MerkleTree.PowerError, fn () -> MerkleTree.makeMerkle(arity_test) end)
   end
 
+  @doc """
+  Test that a failure is thrown when an empty input is given.
+  """
   test "No argument failure" do
     assert_raise(FunctionClauseError, fn () -> MerkleTree.makeMerkle([]) end)
   end

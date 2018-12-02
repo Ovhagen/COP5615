aaeab0ec9163c4ebb9647b5705ebbdaf146cdae1
jotakami <jotakami@gmail.com>
2018-12-02 00:22:34 -0500

    Created blockchain unit test
    Created a stub for unit tests for the Blockchain module

--- proj4/test/Proj4/blockchain_test.exs
+++ proj4/test/Proj4/blockchain_test.exs
@@ -9,24 +9,7 @@ defmodule Proj4.BlockchainTest do
     }
   end
   
-  test "Verify block", data do
-    assert :ok == Block.verify(data.block)
-    
-    assert {:error, :version} == Block.verify(Map.put(data.block, :header, Map.put(data.block.header, :version, 2)))
-    
-    future = DateTime.utc_now |> DateTime.to_unix |> Kernel.+(8_000) |> DateTime.from_unix!
-    assert {:error, :timestamp} == Block.verify(Map.put(data.block, :header, Map.put(data.block.header, :timestamp, future)))
-    
-    assert {:error, :merkle} == Block.verify(Map.put(data.block, :header, Map.put(data.block.header, :merkle_root, :crypto.strong_rand_bytes(32))))
-    
-    assert {:error, :hash} == Block.verify(Map.put(data.block, :header, Map.put(data.block.header, :target, <<0x10100000::32>>)))
-  end
-  
-  test "Serialize and deserialize block", data do
-    assert data.block == Block.deserialize(Block.serialize(data.block))
-  end
-  
-  test "Byte size is calculated correctly", data do
-    assert Block.bytes(data.block) == byte_size(Block.serialize(data.block))
+  test "Verify genesis block", data do
+    assert data.bc.tip.height == 0
   end
 end
